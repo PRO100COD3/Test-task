b0d50773d3032c1a218715f2f1adff70b3115642
PRO100COD3 <vad.dz2015@yandex.ru>
2024-08-27 13:34:42 +0400

    Initial Commit

---
+++ Test-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-task
+//
+//  Created by Вадим Дзюба on 27.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
